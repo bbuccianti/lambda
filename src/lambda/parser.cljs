e823299f9648abf888cd114765ea939b01bb9ee3
Benjamin Buccianti <benjamin@buccianti.dev>
2019-09-12 17:29:05 -0300

    refactor parser for readability

--- src/lambda/parser.cljs
+++ src/lambda/parser.cljs
@@ -1,33 +1,28 @@
 (ns lambda.parser)
 
-(defn find-next [t v from]
+(defn find-next [t v]
   (first
    (map first
         (filter #(= t (second %))
-                (map-indexed vector (subvec v from))))))
+                (map-indexed vector v)))))
 
-(defn find-matching-close
-  ([parens from]
-   (find-matching-close (rest parens) from 0))
+(defn find-matching-close [parens count]
+  (let [tipo (second (first parens))]
+    (cond
+      (and (= :cierra-p tipo) (= 0 count))
+      (first (first parens))
 
-  ([parens from count]
-   (let [tipo (second (first parens))]
-     (cond
-       (and (= :cierra-p tipo) (= 0 count))
-       (first (first parens))
+      (= :abre-p tipo)
+      (find-matching-close (rest parens) (inc count))
 
-       (= :abre-p tipo)
-       (find-matching-close (rest parens) from (inc count))
+      (= :cierra-p tipo)
+      (find-matching-close (rest parens) (dec count)))))
 
-       (= :cierra-p tipo)
-       (find-matching-close (rest parens) from (dec count))))))
-
-(defn find-correct [v from]
+(defn find-correct [v]
   (find-matching-close
-   (rest (filter #(or (= :cierra-p (second %))
-                      (= :abre-p (second %)))
+   (rest (filter #(or (= :cierra-p (second %)) (= :abre-p (second %)))
                  (map-indexed vector v)))
-   from))
+   0))
 
 (defn match [item]
   (case (:tipo item)
@@ -38,18 +33,18 @@
 (defn transform [v]
   (cond
     (and (= (first v) :abre-p) (= (second v) :lambda))
-    (let [punto (find-next :punto v 0)]
+    (let [punto (find-next :punto v)]
       {:abst
        {:param (nth v (dec punto))
         :cuerpo (transform (subvec v (inc punto)))}})
 
     (and (= (first v) :abre-p) (= (last v) :cierra-p))
-    (let [c (find-correct v 0)]
+    (let [c (find-correct (rest v))]
       (if (or (= 5 (count v)) (= 4 (count v)))
        {:apli {:opdor (nth v 1)
                :opndo (nth v 2)}}
-       {:apli {:opdor (transform (subvec v 1 c))
-               :opndo (transform (subvec v (inc c)))}}))
+       {:apli {:opdor (transform (subvec v 1 (inc c)))
+               :opndo (transform (subvec v (+ c 2)))}}))
 
     (and (= (first v) :cierra-p) (= (last v) :cierra-p))
     (if (= 3 (count v))
