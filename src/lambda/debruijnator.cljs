471295ec3e8626eb7fd57c2813eab7d481c0f016
Juan <juanluismunoz.97@gmail.com>
2020-02-25 22:11:05 -0300

    added support for repeated variables, need to add aplication support

--- src/lambda/debruijnator.cljs
+++ src/lambda/debruijnator.cljs
@@ -2,27 +2,51 @@
    (:require
    [clojure.walk :refer [prewalk postwalk prewalk-demo]]))
 
-(defn indexate[exp index]
+(def indexed (atom {}))
+
+(defn- add-indexed[var value]
+   (if (nil? ((keyword var) @indexed))
+    (swap! indexed assoc (keyword var) value)))
+
+(defn- check-indexed[var]
+  ((keyword var) @indexed))
+
+(defn- indexate[exp index]
   (let [param (get-in exp [:param])
         cuerpo (get-in exp  [:cuerpo])
         var (get-in exp  [:param :var])]
-    {:abst
-     {:param
-      (assoc param :index index)
-     :cuerpo
-     (prewalk (fn [target]
-                (if (and (contains? target :var)
-                         (= (target :var) var))
-                  (assoc target :index index)
-                  target))
-              cuerpo)}}))
+    (do
+      (add-indexed var index)
+      {:abst
+       {:param
+        (assoc param :index index)
+        :cuerpo
+        (prewalk (fn [target]
+                   (if (and (contains? target :var)
+                            (= (target :var) var))
+                     (assoc target :index index)
+                     target))
+                 cuerpo)}})))
 
-(defn keep-indexing[exp]
-  (postwalk (fn [target]
-              (if (contains? target :abst)
-                (indexate (target :abst) 1)
-                target))
-            exp))
+(defn- keep-indexing[exp index]
+  (let [index  (atom 0)
+        indexer (fn [target]
+                  (if (contains? target :abst)
+                    (let [used (check-indexed
+                                (get-in target [:abst :param :var]))]
+                      (if (some? used)
+                        (indexate (target :abst) used)
+                        (indexate (target :abst)
+                                  (swap! index inc))))
+                    target))]
+    (postwalk (fn [target]
+                ;; (if (contains? target :apli)
+                ;;   {:apli
+                ;;    {:opdor (indexer (get-in target [:apli :opdor]))
+                ;;     :opndo (indexer (get-in target [:apli :opndo]))}})
+              (indexer target))
+            exp)))
 
 (defn debruijn[exp]
-  (keep-indexing exp))
+  (do (swap! indexed {})
+   (keep-indexing exp 0)))
--- test/lambda/debruijnator_test.cljs
+++ test/lambda/debruijnator_test.cljs
@@ -32,4 +32,79 @@
       {:apli {:opdor {:var "x" :index 1}
               :opndo {:var "y"}}}}}
     "λx.x y"
+
     ))
+
+(deftest multi-level
+  (are [exp act]
+      (= (-> exp) (-> act lex restore parse debruijn))
+    {:abst
+     {:param
+      {:var "x" :index 2}
+     :cuerpo
+      {:abst
+       {:param
+        {:var "y" :index 1}
+        :cuerpo
+        {:var "x" :index 2}}}}}
+    "λx.λy.x"
+
+    {:abst
+     {:param
+      {:var "x" :index 2}
+     :cuerpo
+      {:abst
+       {:param
+        {:var "y" :index 1}
+        :cuerpo
+        {:apli {:opdor {:var "x" :index 2}
+              :opndo {:var "y" :index 1}}}}}}}
+    "λx.λy.x y"
+
+    {:abst
+     {:param {:var "x" :index 4},
+      :cuerpo
+      {:abst
+       {:param {:var "y" :index 3},
+        :cuerpo
+        {:abst
+         {:param {:var "s" :index 2},
+          :cuerpo
+          {:abst
+           {:param {:var "z" :index 1},
+            :cuerpo
+            {:apli
+             {:opdor {:apli {:opdor {:var "x" :index 4}, :opndo {:var "s" :index 2}}},
+              :opndo
+              {:apli
+               {:opdor {:apli {:opdor {:var "y" :index 3}, :opndo {:var "s" :index 2}}},
+                :opndo {:var "z" :index 1}}}}}}}}}}}}}
+    "λx.λy.λs.λz.x s (y s z) "
+    ))
+
+(deftest nested
+  (are [exp act]
+      (= (-> exp) (-> act lex restore parse debruijn))
+    {:apli
+     {:opdor
+      {:abst
+       {:param {:var "x" :index 1},
+        :cuerpo {:apli {:opdor {:var "x" :index 1}, :opndo {:var "x" :index 1}}}}},
+      :opndo
+      {:abst
+       {:param {:var "x" :index 1},
+        :cuerpo {:apli {:opdor {:var "x" :index 1}, :opndo {:var "x" :index 1}}}}}}}
+    "(λx.x x) (λx.x x)"
+
+    {:apli
+     {:opdor
+      {:abst
+       {:param {:var "x" :index 1},
+        :cuerpo {:apli {:opdor {:var "x" :index 1}, :opndo {:var "x" :index 1}}}}},
+      :opndo
+      {:abst
+       {:param {:var "y" :index 1},
+        :cuerpo {:apli {:opdor {:var "y" :index 1}, :opndo {:var "y" :index 1}}}}}}}
+    "(λx.x x) (λy.y y)"
+    ))
+
