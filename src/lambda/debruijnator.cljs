ef7e68f47324949287340683fb8a63a4b0ee8ff6
Juan <juanluismunoz.97@gmail.com>
2020-02-27 22:28:42 -0300

    applications still not working

--- src/lambda/debruijnator.cljs
+++ src/lambda/debruijnator.cljs
@@ -3,50 +3,61 @@
    [clojure.walk :refer [prewalk postwalk prewalk-demo]]))
 
 (def indexed (atom {}))
+(def index (atom 0))
 
-(defn- add-indexed[var value]
-   (if (nil? ((keyword var) @indexed))
-    (swap! indexed assoc (keyword var) value)))
+(defn- add-indexed[var]
+     (swap! indexed assoc (keyword var) @index))
 
-(defn- check-indexed[var]
+(defn- inc-indexed[]
+  (reset! indexed (apply merge 
+                   (map (fn [[k v]] {k (inc v) }) @indexed)))
+  )
+
+(defn- check-index[var]
   ((keyword var) @indexed))
 
-(defn- indexate[exp index]
+(defn- indexate[exp]
   (let [param (get-in exp [:param])
         cuerpo (get-in exp  [:cuerpo])
         var (get-in exp  [:param :var])]
     (do
-      (add-indexed var index)
-      {:abst
-       {:param
-        (assoc param :index index)
-        :cuerpo
-        (prewalk (fn [target]
-                   (if (and (contains? target :var)
-                            (= (target :var) var))
-                     (assoc target :index index)
-                     target))
-                 cuerpo)}})))
+      (swap! index inc)
+      (add-indexed var)
+        {:abst
+         {:param
+          (assoc param :index (check-index var))
+          :cuerpo
+          (prewalk (fn [target]
+                     (if  (and
+                           (contains? target :var)
+                           (nil? (target :index))
+                           (= (target :var) var))
+                       (assoc target :index (check-index var))
+                       target))
+                   cuerpo)}})))
 
 (defn- keep-indexing[exp index]
-  (let [index  (atom 0)
-        indexer (fn [target]
+  (let [indexer (fn [target]
                   (if (contains? target :abst)
-                    (let [used (check-indexed
-                                (get-in target [:abst :param :var]))]
-                      (if (some? used)
-                        (indexate (target :abst) used)
-                        (indexate (target :abst)
-                                  (swap! index inc))))
-                    target))]
+                        (indexate (target :abst))
+                        target))]
+                  ;; (if (some? (get-in target [:opdor]))
+                  ;;   ;(do
+                  ;;    ; (save-level)
+                  ;;    ; (indexate (get-in target [:apli :opdor :abst])))
+                  ;;   (if (some? (get-in target [:opndo]))
+                  ;;    ; (do
+                  ;;       ;(load-level)
+                  ;;     ;  (indexate (get-in target [:apli :opdor :abst])))
+                  ;;     (if (contains? target :abst)
+                  ;;       (indexate (target :abst))
+                  ;;       target))))]
     (postwalk (fn [target]
-                ;; (if (contains? target :apli)
-                ;;   {:apli
-                ;;    {:opdor (indexer (get-in target [:apli :opdor]))
-                ;;     :opndo (indexer (get-in target [:apli :opndo]))}})
-              (indexer target))
-            exp)))
+                (indexer target))
+              exp)))
 
 (defn debruijn[exp]
-  (do (swap! indexed {})
+  (do
+    (swap! indexed {})
+    (reset! index 0)
    (keep-indexing exp 0)))
