f83ab4c0d233cda3d589de5aebcae6adade8f904
Benjamin Buccianti <benjamin@buccianti.dev>
2020-03-05 17:12:35 -0300

    refactor debruijnator

--- src/lambda/debruijnator.cljs
+++ src/lambda/debruijnator.cljs
@@ -2,23 +2,20 @@
    (:require
    [clojure.walk :refer [prewalk postwalk]]))
 
-(defn- indexate [abstraction index]
-  (let [param (:param abstraction)
-        cuerpo (:cuerpo abstraction)
-        var (:var param)]
-    {:abst
-     {:param (assoc param :index index)
-      :cuerpo
-      (prewalk (fn [target]
-                 (if (and (contains? target :var) (= (target :var) var))
-                   (assoc target :index index)
-                   target))
-               cuerpo)}}))
+(defn- indexate [{:keys [param cuerpo]} index]
+  {:abst
+   {:param (assoc param :index index)
+    :cuerpo
+    (prewalk (fn [target]
+               (if (and (contains? target :var) (= target param))
+                 (assoc target :index index)
+                 target))
+             cuerpo)}})
 
 (defn debruijn [expression]
   (let [index (atom 0)]
     (postwalk (fn [target]
                 (if (contains? target :abst)
-                  (indexate (target :abst) (swap! index inc))
+                  (indexate (:abst target) (swap! index inc))
                   target))
               expression)))
