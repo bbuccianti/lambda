ba4074e06e56ccd3af12cf8672129d41c171b82d
Benjamin Buccianti <benjamin@buccianti.dev>
2020-03-22 20:33:04 -0300

    normalize styling

--- src/lambda/normalize.cljs
+++ src/lambda/normalize.cljs
@@ -13,67 +13,62 @@
   (cond
     (= (first lexed) nil) nil
 
-    (= (:tipo (first lexed)) :lambda)
-    (if (= (:tipo (nth lexed 2)) :punto)
+    (= (first lexed) lamb)
+    (if (= (nth lexed 2) point)
       (concat (take 3 lexed) (restore-lambda (drop 3 lexed)))
-      (concat (take 2 lexed)
-              (restore-lambda (concat [point] [lamb] (drop 2 lexed)))))
+      (concat (take 2 lexed) (restore-lambda (concat [point lamb]
+                                                     (drop 2 lexed)))))
 
     :else (concat [(first lexed)] (restore-lambda (rest lexed)))))
 
 (defn next-close-var [lexed counter stack]
   (cond
-    (or (and (= (:tipo (first lexed)) :cierra-p) (= 0 stack))
-        (= nil (first lexed)))
+    (or (and (= (first lexed) cierra) (= 0 stack)) (nil? (first lexed)))
     counter
 
-    (= (:tipo (first lexed)) :cierra-p)
+    (= (first lexed) cierra)
     (next-close-var (rest lexed) (inc counter) (dec stack))
 
-    (= (:tipo (first lexed)) :abre-p)
+    (= (first lexed) abre)
     (next-close-var (rest lexed) (inc counter) (inc stack))
 
-    :else
-    (next-close-var (rest lexed) (inc counter) stack)))
+    :else (next-close-var (rest lexed) (inc counter) stack)))
 
 (defn group-ident [lexed]
   (cond
+    (= (first lexed) lamb)
+    (concat [abre] (restore-abstr lexed) [cierra])
 
-    (= (:tipo (first lexed)) :lambda)
-    (concat [abre]
-                 (restore-abstr lexed)
-                 [cierra])
+    (and (#{:ident :combi} (:tipo (first lexed)))
+         (or (nil? (second lexed)) (= (second lexed) cierra)))
+    lexed
 
-    (and (#{:ident :combi} (:tipo (first lexed))) (or (nil? (second lexed)) (= (second lexed) cierra)))
-      lexed
-      
-
-    (every? #{:ident :combi} (map #(:tipo %) (take 2 lexed)))
-    (if (= (first (nthrest lexed 2)) nil)
+    (every? #{:ident :combi} (map :tipo (take 2 lexed)))
+    (if (nil? (first (drop 2 lexed)))
       [abre (first lexed) (second lexed) cierra]
       (group-ident (concat [abre (first lexed) (second lexed) cierra]
-                           (nthrest lexed 2))))
+                           (drop 2 lexed))))
 
     (and (#{:ident :combi} (:tipo (first lexed)))
          (= abre (second lexed)))
-    (let [corte (next-close-var (nthrest lexed 2) 0 0)
-          sobra (nthrest (nthrest lexed 2) (inc corte))]
+    (let [corte (next-close-var (drop 2 lexed) 0 0)
+          sobra (drop (+ 2 (inc corte)) lexed)]
       (if (nil? (second sobra))
-        (concat [abre] [(first lexed)]
-                (group-ident (take corte (nthrest lexed 2)))
+        (concat [abre (first lexed)]
+                (group-ident (take corte (drop 2 lexed)))
                 [cierra])
         (group-ident
-         (concat [abre] [(first lexed)]
-                 (group-ident (take corte (nthrest lexed 2)))
+         (concat [abre (first lexed)]
+                 (group-ident (take corte (drop 2 lexed)))
                  [cierra] sobra))))
 
     (= (first lexed) abre)
     (let [corte (next-close-var (rest lexed) 0 0)
-          sobra (nthrest (rest lexed) (+ 1 corte))]
+          sobra (drop (inc corte) (rest lexed))]
       (cond
         (nil? (first sobra))
         (concat (group-ident (take corte (rest lexed))))
-        
+
         (#{:ident :combi} (:tipo (first sobra)))
         (if (nil? (second sobra))
           (concat [abre] (group-ident (take corte (rest lexed)))
@@ -83,7 +78,7 @@
 
         (= (first sobra) abre)
         (let [corte2 (next-close-var (rest sobra) 0 0)
-              sobra2 (nthrest (rest sobra) (+ 1 corte2))]
+              sobra2 (drop (inc corte2) (rest sobra))]
           (if (nil? (first sobra2))
             (concat [abre] (group-ident (take corte (rest lexed)))
                     (group-ident (take corte2 (rest sobra)))
@@ -100,13 +95,12 @@
 
     (= (first lexed) point)
     (let [corte (next-close-var (rest lexed) 0 0)
-          sobra (nthrest (rest lexed) (+ 1 corte))]
+          sobra (drop (inc corte) (rest lexed))]
      (if (some? (first sobra))
-        (concat [(first lexed)]
-                (group-ident (take corte (rest lexed)))
-                (restore-abstr sobra))
-        (concat [(first lexed)]
-                (group-ident (take corte (rest lexed))))))
+       (concat [(first lexed)]
+               (group-ident (take corte (rest lexed)))
+               (restore-abstr sobra))
+        (concat [(first lexed)] (group-ident (take corte (rest lexed))))))
 
     :else
     (concat [(first lexed)] (restore-abstr (rest lexed)))))
@@ -118,14 +112,13 @@
 
     (= (first lexed) abre)
     (if (= (second lexed) lamb)
-      (concat [(first lexed)] [(second lexed)]
-              (restore-exp (nthrest lexed 2)))
+      (concat [(first lexed) (second lexed)] (restore-exp (drop 2 lexed)))
       (concat [(first lexed)] (restore-exp (rest lexed))))
 
     (= (first lexed) lamb)
     (let [corte (next-close-var lexed 0 0)]
-      (concat [abre] [(first lexed)] (restore-exp (take corte (rest lexed)))
-              [cierra] (restore-exp (nthrest (rest lexed) corte))))
+      (concat [abre (first lexed)] (restore-exp (take corte (rest lexed)))
+              [cierra] (restore-exp (drop corte (rest lexed)))))
 
     :else (concat [(first lexed)] (restore-exp (rest lexed)))))
 
