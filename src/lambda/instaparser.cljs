d059f21e7fa10f2d762e0ff57a8995a409608c4a
Benjamin Buccianti <benjamin@buccianti.dev>
2020-04-27 22:17:23 -0300

    fix error messages

--- src/lambda/instaparser.cljs
+++ src/lambda/instaparser.cljs
@@ -1,5 +1,6 @@
 (ns lambda.instaparser
   (:require
+   [clojure.string :refer [replace join]]
    [instaparse.core :as insta :refer-macros [defparser]]
    [lambda.state :as state]
    [lambda.lexer :refer [lex]]
@@ -11,7 +12,7 @@
 (defparser lambda-parser
   "expr = var | <whitespace>* abstraction | <whitespace>* application
     application = <'('> <whitespace>* expr <whitespace>* expr <whitespace>* <')'> | (<whitespace>* expr)*
-    abstraction = <'λ'> (<whitespace>* ident)* <'.'> body
+    abstraction = <'λ'> (<whitespace>* ident <whitespace>*)+ <'.'> body
     whitespace = #'\\s+'
     body = expr
     ident = #'[a-z]+'
@@ -21,6 +22,13 @@
   (and (not (empty? @state/command))
        (insta/failure? (lambda-parser @state/command))))
 
+(defn expectings [failure]
+  (let [failures (map (comp str :expecting) (-> failure :reason))
+        cleaned (remove #(= (str %) "/^\\s+/") failures)]
+    (-> (str (-> cleaned (interleave (repeat "o")) butlast ((partial join " "))))
+        (replace "/^[a-z]+/" "un parámetro")
+        (replace "/^[A-Za-z]+/" "un identificador"))))
+
 (defn help-label []
   (let [failure (insta/get-failure (lambda-parser @state/command))]
     (when (failure?)
@@ -28,5 +36,5 @@
        {:pointing "above"
         :size "big"
         :color "red"}
-       (str "¿Falta un '" (-> failure :reason first :expecting)
-            "' cerca de la posición " (-> failure :column) "?")])))
+       (str "¿Falta " (expectings failure)
+            " cerca de la posición " (-> failure :column) "?")])))
