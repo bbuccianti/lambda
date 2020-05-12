f0640ce2ca0218ed0eaee9a36fe31e8ba781696f
Benjamin Buccianti <benjamin@buccianti.dev>
2020-05-12 20:12:24 -0300

    add ultra for better tests output

--- project.clj
+++ project.clj
@@ -37,7 +37,8 @@
                          [figwheel-sidecar "0.5.19"]
                          [cider/piggieback "0.4.1"]
                          [nrepl "0.7.0-alpha3"]]
-         :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
+         :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
+                   [venantius/ultra "0.6.0"]]
          :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
          :source-paths  ["src" "dev" "test"]
          :clean-targets ^{:protect false}
