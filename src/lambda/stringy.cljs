322eabaea1c6311b52ba7864e35bee583ab78661
Benjamin Buccianti <benjamin@buccianti.dev>
2020-05-11 21:13:50 -0300

    show parens when opndo needs it

--- src/lambda/stringy.cljs
+++ src/lambda/stringy.cljs
@@ -2,23 +2,25 @@
   (:require
    [lambda.state :as state]))
 
-(defn toString [m]
-  (let [full? (:full? @state/config)]
-    (cond
-      (contains? m :abst)
-      (let [abst (:abst m)]
-        (str "(λ" (toString (:param abst)) "."
-             (toString (:cuerpo abst)) ")"))
+(defn toString
+  ([m] (toString m false))
+  ([m flag]
+   (let [full? (:full? @state/config)]
+     (cond
+       (contains? m :abst)
+       (let [abst (:abst m)]
+         (str "(λ" (toString (:param abst) flag) "."
+              (toString (:cuerpo abst) flag) ")"))
 
-      (contains? m :apli)
-      (let [apli (:apli m)
-            church-number? (= "f" (get-in apli [:opdor :var]))]
-        (str (if (or full? church-number?) "(" "")
-             (toString (:opdor apli))
-             " "
-             (toString (:opndo apli))
-             (if (or full? church-number?) ")" "")))
+       (contains? m :apli)
+       (let [apli (:apli m)
+             needed? (get-in apli [:opndo :apli] false)]
+         (str (if (or full? flag) "(" "")
+              (toString (:opdor apli) flag)
+              " "
+              (toString (:opndo apli) needed?)
+              (if (or full? flag) ")" "")))
 
-      (or (contains? m :var) (contains? m :ident))
-      (let [kw (if (contains? m :var) :var :ident)]
-        (str (kw m) (when (contains? m :index) (str "_" (:index m))))))))
+       (or (contains? m :var) (contains? m :ident))
+       (let [kw (if (contains? m :var) :var :ident)]
+         (str (kw m) (when (contains? m :index) (str "_" (:index m)))))))))
